ff354c4e9efc9694e5f913fb001ca30582c310d4
UmbertoZuk <64486813+UmbertoZuk@users.noreply.github.com>
2020-05-22 17:32:19 +0200

    nulla di importante
    sistemati i membri di alcune funzioni e qualche ininfluente dettaglio qua e là

--- Test/Clamped_torsion.m
+++ Test/Clamped_torsion.m
@@ -52,11 +52,32 @@ beam.name='ClampedTorsionBeam'
 for i = 2:nel+1
     beam.in(i).d = u(1+6*(i-2):6*(i-1),1);
 end
-options.plot        = 1;
-options.plotColor   = 'green';
-options.saveSTL     = 0;
-options.ovs         = 4;
-[fig] = b_plot3d(beam, options);
+% options.plot        = 1;
+% options.plotColor   = 'green';
+% options.saveSTL     = 0;
+% options.ovs         = 4;
+% [fig] = b_plot3d(beam, options);
+
+
+close all
+
+torsion3d=m_init;
+torsion3d.en=en_ground([0 0 0]');
+beam.o=[0 0 0]';
+beam.vx=[0 1 0]';
+beam.vy=[1 0 0]';
+
+torsion3d=m_add_beam(torsion3d,beam)
+% 3dtorsion=m_compute_matrices(3dtorsion)
+
+
+   
+options.plot_original          = 1;
+options.plot_deformed          = 1;
+options.plotColor              = 'green';
+options.saveSTL                = 0;
+options.point_section          = 8;
+[fig] = m_plot3d(torsion3d,options)
 
 % return where I come from ... home sweet home
 cd ./test
