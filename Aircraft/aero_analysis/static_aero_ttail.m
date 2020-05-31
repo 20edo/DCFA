bd8e0f0855e72f1f89ec5a4d0911b87bc761063a
Matteo-Zemello <64530115+Matteo-Zemello@users.noreply.github.com>
2020-05-31 16:09:33 +0200

    Plot and analysis divergence + m_plot_eigenshape
    and deleting some old functions

--- Aircraft/aero_analysis/static_aero_ttail.m
+++ Aircraft/aero_analysis/static_aero_ttail.m
@@ -1,4 +1,5 @@
 % Static aero analysis of the T-Tail
+% - Divergence
 %%
 % DCFA swept wing assignement
 %
@@ -11,45 +12,88 @@
 %
 clear all, close all, clc 
 cd ..
-% generate the wing model
+%% Generate the aircraft model
 cd generate_model
 generate_model
 % Move to the analysis folder
 cd aero_analysis\
 
 
-% Build the wing model
-
+%% Build the wing model
 ttail=m_init();
 ttail.en=[en_ground(aircraft.en(5).x)];
 ttail_list=[aircraft.b(13) aircraft.b(14) aircraft.b(15)];
-  
 for i=1:length(ttail_list)
     ttail=m_add_beam(ttail,ttail_list(i));
 end
 
-% Add the aero loads
+%% Add the aero loads
 ttail = m_add_aero_loads(ttail,[1,0,0]');
 
-% Find the divergence dynamic pressure
-q_div = eigs(ttail.K,ttail.Ka,30,'smallestabs');
-q_div = sort(real(q_div));
-q_div(q_div<0)=[]; 
-q_div = q_div(1)
+%% Find the divergence dynamic pressure
 
+[V,D]= eigs(ttail.K,ttail.Ka,30,'smallestabs');
+q_div = diag(D); 
+[q_div,I] = sort(real(q_div)); 
+V = V(:,I);                         % sort the eigenshapes
+V(:,q_div<0)=[];                    % select the eigenshapes with positive eig
+q_div(q_div<0)=[]; 
+q_div = q_div(1);                   % select the minimum positive q_inf
+V_div = V(:,1);                     % select its eigenshape
+
+%% Plot of the divergence eigenshape 
+if 1
+    options.plot_original          = 1;
+    options.plot_deformed          = 1;
+    options.plotColor              = 'green';
+    options.saveSTL                = 0;
+    options.point_section          = 8;
+    options.N                      = 1;         % we have only one eig
+    m_plot_eigenshape(ttail,options,1/4*V_div)
+end
+
+%% Calculations for the plotting VTAS and MACH when altitude changes
 [T, a, P, rho] = atmosisa(0:100:11000); 
 v = sqrt(q_div*2./rho); 
 M = v./a; 
 
+%% Plote and save results 
+
+figure(2)
+title('Static divergence T-Tail modeshape')
+fig = figure(2)
+for h = 1:4
+    if h==1
+        fname = ['Static_Divergence_T-Tail_view3D'];
+        saveas(fig,fname,'svg')
+    elseif h == 2
+        view([1 0 0])
+        fname = ['Static_Divergence_T-Tail_viewX'];
+        saveas(fig,fname,'svg')
+    elseif h==3
+        view([0 1 0])
+        fname = ['Static_Divergence_T-Tail_viewY'];
+        saveas(fig,fname,'svg')
+    elseif h==4
+        view([0 0 1])
+        fname = ['Static_Divergence_T-Tail_viewZ'];
+        saveas(fig,fname,'svg')
+    end
+end
+
+
+figure(3)
+set(gcf, 'Position',  [40, 40, 700, 500])
 subplot(1,2,1)
-    plot(0:100:11000,v)
+    plot(0:100:11000,v,'LineWidth',2)
     grid on
     xlabel('Altitude [m]')
-    ylabel('Divergence Velocity [m/s]')
-    title('Divergence Velocity - Altitude')
+    ylabel('Divergence VTAS [m/s]')
+    title('Divergence VTAS T-Tail')
 subplot(1,2,2)
-    plot(0:100:11000,M)
+    plot(0:100:11000,M,'LineWidth',2)
     grid on
     xlabel('Altitude [m]')
     ylabel('Divergence Mach [-]')
-    title('Divergence Mach - Altitude')
+    title('Divergence Mach T-Tail')
+saveas(figure(3),'Static_divergence_T-Tail_graph','svg')
