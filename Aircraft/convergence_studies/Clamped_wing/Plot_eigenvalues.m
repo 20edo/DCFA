32582027f166d71216cace45e5a49c090dfecc90
20eddy <61024509+20eddy@users.noreply.github.com>
2020-06-19 12:36:15 +0200

    plot_update

--- Aircraft/convergence_studies/Clamped_wing/Plot_eigenvalues.m
+++ Aircraft/convergence_studies/Clamped_wing/Plot_eigenvalues.m
@@ -15,10 +15,11 @@ clear all, close all, clc
 
 load w_esatta.mat
 
+f_esatta=w_esatta/2/pi;
 fig=figure;
-semilogy(1:length(w_esatta),w_esatta,'xr')
+semilogy(1:length(f_esatta),f_esatta,'xr')
 % title('Clamped wing')
-xlabel('Number of eigenvalue')
-ylabel('Frequency')
+xlabel('Number of eigenvalue', 'interpreter', 'latex')
+ylabel('Frequency \quad [Hz]', 'interpreter', 'latex')
 grid on
 saveas(fig,'Eigenvalues_clamped_wing','epsc')
--- Aircraft/convergence_studies/Free_fuselage/Plot_eigenvalues.m
+++ Aircraft/convergence_studies/Free_fuselage/Plot_eigenvalues.m
@@ -15,9 +15,10 @@ clear all, close all, clc
 
 load w_esatta.mat
 
+f_esatta=w_esatta/2/pi;
 fig=figure;
 semilogy(1:length(w_esatta)+6,[0 0 0 0 0 0 w_esatta(1:end)'],'xr')
-xlabel('Number of eigenvalue')
-ylabel('Frequency')
+xlabel('Number of eigenvalue', 'interpreter', 'latex')
+ylabel('Frequency \quad [Hz]', 'interpreter', 'latex')
 grid on
 saveas(fig,'Eigenvalues_free_fuselage','epsc')
